b2fb9f3129dc5b031668208e313295ac218f997c
garlicsuter <markasuter@gmail.com>
2020-07-21 11:59:16 -0400

    new shapes

---
+++ Assets/Scenes/notes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notes : MonoBehaviour
+{
+    // Week2 Lesson 2:
+    //    - New Project
+    //    - New Cub X+Z scale 40
+    //    - Import FP Controller
+    // Week3 Lesson 2:
+    //    - New "Models Folder"
+    //    - Download(& Extract) and Import Models
+    //    - Delete old Cube
+    //    - Drag in "museum" model, scale 10 x 10 x 10
+
+}
