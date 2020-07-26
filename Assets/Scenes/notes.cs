78fbe6fdfbbf0e676c4e00d3d0f1f89d0f399dc7
garlicsuter <markasuter@gmail.com>
2020-07-26 19:03:53 -0400

    else distance...no Title, Author, Description

--- Assets/Scenes/notes.cs
+++ Assets/Scenes/notes.cs
@@ -8,10 +8,23 @@ public class notes : MonoBehaviour
     //    - New Project
     //    - New Cub X+Z scale 40
     //    - Import FP Controller
+
     // Week3 Lesson 2:
     //    - New "Models Folder"
     //    - Download(& Extract) and Import Models
     //    - Delete old Cube
     //    - Drag in "museum" model, scale 10 x 10 x 10
+    //    - Add Mesh Colliders
+    //    - Adjust Lights (No Shadows, Down on Y Axis)
+    //    - Move FirstPerson controller into Welcome room
+    //    - Add Physics Materials to rooms
+    //    - Add sound effects to First Person Controller to play correct sound
 
+    // Week4 Lesson 2:
+    //    - Text (GameObject > UI > Text) x 3 Title, Author, Description
+    //    - In "Textures" Folder, import JPEG's
+    //    - New plane, drag JPEG onto plane IN HIERARCHY (Sometimes won't drag onto scene view plane)
+    //    - Adjust all lights intensity up to 2. Disable Directional (Sun) Light
+    //    - 3.3 Proximity Script: Gotchya: I forgot to apply tags to texts Title, Auth, Description
+    //    - 3.3 Proximity Script: Gotchya 2: Text does not disappear after moving away from art
 }
---
+++ Assets/Scripts/Proximity.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Proximity : MonoBehaviour
+{
+    public string newTitle;
+    public string newAuthor;
+    public string newDesc;
+    private Transform other;
+    private Text myTitle;
+    private Text myAuthor;
+    private Text myDesc;
+    private float dist;
+    private GameObject player;
+    private GameObject message1;
+    private GameObject message2;
+    private GameObject message3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        other = player.GetComponent<Transform>();
+        message1 = GameObject.FindWithTag("ArtTitle");
+        message2 = GameObject.FindWithTag("Auth");
+        message3 = GameObject.FindWithTag("Description");
+        myTitle = message1.GetComponent<Text>();
+        myTitle.text = "";
+        myAuthor = message2.GetComponent<Text>();
+        myAuthor.text = "";
+        myDesc = message3.GetComponent<Text>();
+        myDesc.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (other)
+        {
+           dist = Vector3.Distance(transform.position, other.position);
+            print("Distance to player: " + dist);
+            if (dist < 4)
+            {
+                myTitle.text = newTitle;
+                myAuthor.text = newAuthor;
+                myDesc.text = newDesc;
+            }
+
+            else {
+                myTitle.text = "";
+                myAuthor.text = "";
+                myDesc.text = "";
+            }
+          
+        }
+
+    }
+}
