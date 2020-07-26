2618e703fa334d832f05b24b19ba674b1aff4029
garlicsuter <markasuter@gmail.com>
2020-07-26 19:04:43 -0400

    Update notes.cs

--- Assets/Scenes/notes.cs
+++ Assets/Scenes/notes.cs
@@ -26,5 +26,5 @@ public class notes : MonoBehaviour
     //    - New plane, drag JPEG onto plane IN HIERARCHY (Sometimes won't drag onto scene view plane)
     //    - Adjust all lights intensity up to 2. Disable Directional (Sun) Light
     //    - 3.3 Proximity Script: Gotchya: I forgot to apply tags to texts Title, Auth, Description
-    //    - 3.3 Proximity Script: Gotchya 2: Text does not disappear after moving away from art
+    //    - 3.3 Proximity Script: Gotchya 2: Text does not disappear after moving away from art...Fix with else set text to empty strings
 }
